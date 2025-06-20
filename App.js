1197720178e44fa855a00223eed382620b45ecca
rayncode-maker <abhilasray16@gmail.com>
2025-06-20 22:41:25 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import InventoryList from './components/InventoryList';
+
+function App() {
+  return (
+    <div className="App">
+      <InventoryList />
+    </div>
+  );
+}
+
+export default App;
